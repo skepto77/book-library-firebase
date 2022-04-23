3bc475b3cfa1f57949915a28875fc21ecce41b05
Ilya Grachev <ilya095@gmail.com>
2022-04-23 11:14:24 +0300

    upd firebase init

--- src/main.ts
+++ src/main.ts
@@ -1,29 +1,19 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import * as admin from 'firebase-admin';
-import { ServiceAccount } from 'firebase-admin';
 import { ValidationPipe } from '@nestjs/common';
+import * as serviceAccount from '../firebaseServiceAccountKey.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
 
-  const configService: ConfigService = app.get(ConfigService);
-  const adminConfig: ServiceAccount = {
-    projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
-    privateKey: configService
-      .get<string>('FIREBASE_PRIVATE_KEY')
-      .replace(/\\n/g, '\n'),
-    clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-  };
-
+  const config: object = serviceAccount;
   admin.initializeApp({
-    credential: admin.credential.cert(adminConfig),
+    credential: admin.credential.cert(config),
     databaseURL:
       'https://netology-backend-default-rtdb.europe-west1.firebasedatabase.app/',
   });
-
-  await app.listen(configService.get<string>('API_PORT') || 3000);
+  await app.listen(3000);
 }
 bootstrap();
